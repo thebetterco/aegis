69da6cebe26beb63a30f570442ad547fe4112c9a
thebetterco <150898176+thebetterco@users.noreply.github.com>
2025-09-01 19:57:12 +0900

    Define web middleware group

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,22 +8,35 @@ class Kernel extends HttpKernel
 {
     /**
      * The application's global HTTP middleware stack.
-     *
-     * @var array
      */
-    protected $middleware = [];
+    protected $middleware = [
+        // Add global middleware here if needed
+    ];
 
     /**
      * The application's route middleware groups.
-     *
-     * @var array
      */
-    protected $middlewareGroups = [];
+    protected $middlewareGroups = [
+        'web' => [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
 
     /**
      * The application's route middleware.
-     *
-     * @var array
      */
-    protected $routeMiddleware = [];
+    protected $routeMiddleware = [
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'superadmin' => \App\Http\Middleware\EnsureSuperAdmin::class,
+    ];
 }
